47a180d3d4928decee665cf6d51e40b61c39fb1d
Amit232 <potterboy232@gmail.com>
2017-03-11 02:32:33 +0530

    Modified utility for getting avergae stars

--- util/books_data_from_google.py
+++ util/books_data_from_google.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!python
 import urllib, json, cStringIO
 from openpyxl import Workbook
 # from PIL import Image
@@ -27,6 +27,48 @@ def get_book_title_from_txt_file(path_to_file):
     print book_title_list
     return book_title_list
 
+def get_author_name(data):
+    try:
+        return data["items"][0]["volumeInfo"]["authors"][0].encode(
+            'ascii', 'ignore')
+    except:
+        return "Not Available"
+
+def get_book_description(data):
+    try:
+        return data["items"][0]["volumeInfo"]["description"].encode(
+            'ascii', 'ignore')
+    except:
+        return "Not Available"
+
+def get_isbn(data):
+    return data["items"][0]["volumeInfo"]["industryIdentifiers"][0][
+                "identifier"].encode('ascii', 'ignore')
+
+def get_category(data):
+    try:
+        return data["items"][0]["volumeInfo"]["categories"][0]
+    except:
+        return "Not Available"
+
+def get_image_url(data):
+    try:
+        return data["items"][0]["volumeInfo"]["imageLinks"][
+                "thumbnail"]
+    except:
+        return "Not Available"
+
+def get_average_rating(data):
+    try:
+        return data["items"][0]["volumeInfo"]["averageRating"]
+    except:
+        return "Not Available"
+
+def get_page_count(data):
+    try:
+        return data["items"][0]["volumeInfo"]["pageCount"]
+    except:
+        return "Not Available"
 
 def get_book_metadata():
     # Dictionary with book title as key and related information such as author, isbn, category, description etc..
@@ -37,25 +79,18 @@ def get_book_metadata():
         request = service.volumes().list(source='public', q=book_title)
         response = request.execute()
         # time.sleep(1)
-        "Reading json data after sleep"
+        # print "Reading json data after sleep"
         data = response
         print type(data)
-        with open("json_data_from_ggogle.txt", "w") as json_file_handler:
-            json_file_handler.write(str(data))
-        # print(data)
         print "JSON data loaded"
         try:
-            books_metadata_dictionary[book_title]["author"] = data["items"][0]["volumeInfo"]["authors"][0].encode(
-                'ascii', 'ignore')
-            books_metadata_dictionary[book_title]["description"] = data["items"][0]["volumeInfo"]["description"].encode(
-                'ascii', 'ignore')
-            books_metadata_dictionary[book_title]["isbn"] = data["items"][0]["volumeInfo"]["industryIdentifiers"][0][
-                "identifier"].encode('ascii', 'ignore')
-            books_metadata_dictionary[book_title]["category"] = data["items"][0]["volumeInfo"]["categories"][0]
-            books_metadata_dictionary[book_title]["image_url"] = data["items"][0]["volumeInfo"]["imageLinks"][
-                "thumbnail"]
-            books_metadata_dictionary[book_title]["average_rating"] = data["items"][0]["volumeInfo"]["averageRating"]
-            books_metadata_dictionary[book_title]["pageCount"] = data["items"][0]["volumeInfo"]["pageCount"]
+            books_metadata_dictionary[book_title]["author"] = get_author_name(data)
+            books_metadata_dictionary[book_title]["description"] = get_book_description(data)
+            books_metadata_dictionary[book_title]["isbn"] = get_isbn(data)
+            books_metadata_dictionary[book_title]["category"] = get_category(data)
+            books_metadata_dictionary[book_title]["image_url"] = get_image_url(data)
+            books_metadata_dictionary[book_title]["average_rating"] = get_average_rating(data)
+            books_metadata_dictionary[book_title]["pageCount"] = get_page_count(data)
 
         except Exception as e:
             print "Exception occured ", e
@@ -82,14 +117,18 @@ def dump_metadata_to_excel_file():
     print final_dictionary.keys()
     print sheet.max_row
     for row, key in zip(range(2, 200), final_dictionary.keys()):
-        sheet["A" + str(row)] = key
-        sheet["B" + str(row)] = final_dictionary[key]["author"]
-        sheet["C" + str(row)] = final_dictionary[key]["description"]
-        sheet["D" + str(row)] = final_dictionary[key]["category"]
-        sheet["E" + str(row)] = final_dictionary[key]["isbn"]
-        sheet["F" + str(row)] = final_dictionary[key]["image_url"]
-        sheet["G" + str(row)] = final_dictionary[key]["average_rating"]
-        sheet["H" + str(row)] = final_dictionary[key]["pageCount"]
+        try:
+            sheet["A" + str(row)] = key
+            sheet["B" + str(row)] = final_dictionary[key]["author"]
+            sheet["C" + str(row)] = final_dictionary[key]["description"]
+            sheet["D" + str(row)] = final_dictionary[key]["category"]
+            sheet["E" + str(row)] = final_dictionary[key]["isbn"]
+            sheet["F" + str(row)] = final_dictionary[key]["image_url"]
+            # sheet["G" + str(row)] = final_dictionary[key]["average_rating"]
+            sheet["H" + str(row)] = final_dictionary[key]["pageCount"]
+        except Exception as e:
+            print("Exception ", e , " occured for " + key)
+            continue
 
     wb.save("Metadata_134_1.xlsx")
 dump_metadata_to_excel_file()
